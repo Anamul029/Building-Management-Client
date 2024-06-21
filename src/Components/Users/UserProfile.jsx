7ac4952a98ac3129bdf153a8763f0ae2f98c52a9
Anamul029 <anamhaque.5101@gmail.com>
2024-06-21 10:25:01 +0600

    manage member profile

--- src/Components/Users/UserProfile.jsx
+++ src/Components/Users/UserProfile.jsx
@@ -6,21 +6,29 @@ import UseAxiosSecure from "../Hooks/UseAxiosSecure";
 
 const UserProfile = () => {
     const [role] = UseRoll();
-    const [rooms]=UseRooms()
+    const [rooms] = UseRooms()
     const { user } = useContext(AuthContext)
-    const[users,setUsers]=useState([])
-    const axiosSecure=UseAxiosSecure();
-    useEffect(()=>{
+    const [users, setUsers] = useState([])
+    const axiosSecure = UseAxiosSecure();
+    const [Member,setMember]=useState()
+    useEffect(() => {
         axiosSecure.get('/users')
-        .then(res=>{
-            setUsers(res.data)
-        })
-    },[axiosSecure,users])
-    const totalUsers=users.filter(user=>user.role==='guest');
-    const totalMembers=users.filter(user=>user.role==='member');
-    const availableRoom=((rooms.length -totalMembers.length)/rooms.length)*100;
-    const agreementRoom=(totalMembers.length/rooms.length)*100;
-    
+            .then(res => {
+                setUsers(res.data)
+            })
+    }, [axiosSecure, users])
+    const totalUsers = users.filter(user => user.role === 'guest');
+    const totalMembers = users.filter(user => user.role === 'member');
+    const availableRoom = ((rooms.length - totalMembers.length) / rooms.length) * 100;
+    const agreementRoom = (totalMembers.length / rooms.length) * 100;
+    // get room information for member
+    useEffect(() => {
+        axiosSecure.get(`/booking/${user?.email}`)
+            .then(res => {
+                setMember(res.data)
+            })
+    }, [axiosSecure, user])
+
     if (role === 'admin') {
         return (
             <div className=" p-5 bg-blue-50 min-h-screen text-center">
@@ -39,7 +47,24 @@ const UserProfile = () => {
                 <h3 className="Md:text-3xl md:font-semibold">Percentage of Agreement Rooms: {agreementRoom}%</h3>
                 <h3 className="Md:text-3xl md:font-semibold">Total Numbers of Users: {totalUsers.length}</h3>
                 <h3 className="Md:text-3xl md:font-semibold">Total Numbers of Members: {totalMembers.length}</h3>
-                
+
+            </div>
+        );
+    }
+    else if (role === 'guest') {
+        return (
+            <div className=" p-5 bg-blue-50 min-h-screen text-center">
+                <h2 className="text-4xl font-semibold mb-8 underline my-12">My Profile Information</h2>
+                <div>
+                    <div className="avatar">
+                        <div className="w-24 md:w-52  rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img className="" src={user.photoURL} />
+                        </div>
+                    </div>
+                </div>
+                <h3 className="md:text-3xl md:font-semibold">Email: {user.email}</h3>
+                <h3 className="md:text-3xl md:font-semibold">Name: {user.displayName}</h3>
+            
             </div>
         );
     }
@@ -55,6 +80,7 @@ const UserProfile = () => {
                     </div>
                 </div>
                 <h3 className="md:text-3xl md:font-semibold">Email: {user.email}</h3>
+                <h3 className="md:text-3xl md:font-semibold">Name: {user.displayName}</h3>
                 <h3 className="md:text-3xl md:font-semibold">Name: {user.displayName}</h3>
             </div>
         );
